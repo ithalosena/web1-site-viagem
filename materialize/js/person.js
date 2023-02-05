4edfae1ae452f6e652bfc6608a330e3764e32a5a
ithalosena <86742633+ithalosena@users.noreply.github.com>
2023-02-05 14:44:48 -0300

    update index
    mexi no carrossel de imagens

--- materialize/js/person.js
+++ materialize/js/person.js
@@ -2,4 +2,14 @@ $(document).ready(function(){
     $('.carousel').carousel();
     $(".dropdown-trigger").dropdown();
   });
-      
+  document.addEventListener('DOMContentLoaded', function() {
+    var elems = document.querySelectorAll('.carousel');
+    var instances = M.Carousel.init(elems, {
+      fullWidth: true,
+      indicators: true
+    });
+
+    setInterval(function() {
+      instances[0].next();
+    }, 5000);
+  });      
