0024869ba2310e2789b99da5249450c32d9517d5
ithalosena <86742633+ithalosena@users.noreply.github.com>
2023-02-07 19:24:09 -0300

    tudo que eu fiz ate a aula de terça

--- materialize/js/person.js
+++ materialize/js/person.js
@@ -1,15 +1,39 @@
-$(document).ready(function(){
-    $('.carousel').carousel();
-    $(".dropdown-trigger").dropdown();
+$(document).ready(function () {
+  $('.carousel').carousel();
+  $(".dropdown-trigger").dropdown();
+});
+document.addEventListener('DOMContentLoaded', function () {
+  var elems = document.querySelectorAll('.carousel');
+  var instances = M.Carousel.init(elems, {
+    fullWidth: true,
+    indicators: true
   });
-  document.addEventListener('DOMContentLoaded', function() {
-    var elems = document.querySelectorAll('.carousel');
-    var instances = M.Carousel.init(elems, {
-      fullWidth: true,
-      indicators: true
-    });
 
-    setInterval(function() {
-      instances[0].next();
-    }, 5000);
-  });      
+  setInterval(function () {
+    instances[0].next();
+  }, 5000);
+});
+
+document.addEventListener('DOMContentLoaded', function () {
+  var elems = document.querySelectorAll('.fixed-action-btn');
+  var instances = M.FloatingActionButton.init(elems, {
+    toolbarEnabled: true
+  });
+});
+
+// Or with jQuery
+
+$('.fixed-action-btn').floatingActionButton({
+  toolbarEnabled: true
+});
+
+document.addEventListener('DOMContentLoaded', function () {
+  var elems = document.querySelectorAll('select');
+  var instances = M.FormSelect.init(elems, options);
+});
+
+// Or with jQuery
+
+$(document).ready(function () {
+  $('select').formSelect();
+});
